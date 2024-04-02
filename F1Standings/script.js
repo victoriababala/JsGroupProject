6a4f25138ba9421626371a5688423701dd37455f
victoriababala <victoriababala1106@gmail.com>
2024-04-02 23:46:26 +0300

    fixed styling

--- F1Standings/script.js
+++ F1Standings/script.js
@@ -5,30 +5,36 @@ $(document).ready(function () {
       type: "GET",
       dataType: "json",
       success: function (data) {
-        var standingsHTML = "<h2>Team Standings</h2>";
+        var standingsHTML = '<div class="container">';
+        standingsHTML += '<h1 class="mt-4 text-center">Teams Standings</h1>';
         $.each(data, function (index, team) {
-          standingsHTML += '<div class="team-info">';
-          standingsHTML += "<h3>" + team.name + "</h3>";
-          standingsHTML += "<p>Rank: " + team.rank + "</p>";
-          standingsHTML += "<p>Points: " + team.points + "</p>";
+          standingsHTML += '<div class="row team-info shadow border p-3 mb-4 rounded border-secondary ">';
+          standingsHTML += '<div class="col-md-6">';
+          standingsHTML += '<h2 class="mt-3 ">' + team.rank + "</h2>";
+          standingsHTML += '<h3 class="mt-4 " >' + team.name + "</h3>";
+          standingsHTML += '<h3 class="mt-3 ">' + team.points + " PTS</h3>";
+          standingsHTML += "</div>";
+          standingsHTML += '<div class="col-md-6 rounded ">';
           standingsHTML +=
-            '<img src="' +
+            '<img style="max-width:20%;" src="' +
             team["logo-small"] +
             '" alt="' +
             team.name +
-            ' Logo" class="team-logo">';
+            ' Logo" class="team-logo img-fluid float-right">';
           standingsHTML +=
-            '<img src="' +
+            '<img  src="' +
             team["image-car"] +
             '" alt="' +
             team.name +
-            ' Car" class="car-image">';
+            ' Car" class="car-image img-fluid">';
           standingsHTML += "</div>";
+          standingsHTML += "</div>"; // Close row
         });
+        standingsHTML += "</div>"; // Close container
         $("#teamStandingsContainer").html(standingsHTML);
-        $("#carouselExampleControls").hide(); // Hide the carousel
+        $("#carouselExampleControls").hide();
+        $("#title").hide(); // Hide the title
       },
     });
   });
 });
-
